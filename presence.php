8e800b9a9f69892dd3ccd938bd88e09b6d8fe9ba
osm-zrl <78822717+osm-zrl@users.noreply.github.com>
2023-05-15 00:35:50 +0100

    modified:   head.php 	modified:   presence.php

--- presence.php
+++ presence.php
@@ -1,6 +1,34 @@
 <!DOCTYPE html>
 
 <html lang="en">
+<?php
+require('dbconfig.php');
+
+// prepare the SQL statement to count the total number of students
+$count_students = "SELECT COUNT(*) as total_students FROM student";
+
+// execute the SQL statement to count the total number of students and store the result
+$count_result = $conn->query($count_students);
+$count_row = $count_result->fetch_assoc();
+$total_students = $count_row['total_students'];
+
+// prepare the SQL statement to count the total number of classes
+$count_classes = "SELECT COUNT(DISTINCT class_id) as total_classes FROM classe";
+
+// execute the SQL statement to count the total number of classes and store the result
+$count_result = $conn->query($count_classes);
+$count_row = $count_result->fetch_assoc();
+$total_classes = $count_row['total_classes'];
+
+// prepare the SQL statement to count the total number of classes
+$count_classes = "SELECT COUNT(DISTINCT card_id) as total_cards FROM cards";
+
+// execute the SQL statement to count the total number of classes and store the result
+$count_result = $conn->query($count_classes);
+$count_row = $count_result->fetch_assoc();
+$total_cards = $count_row['total_cards'];
+
+?>
 
 <head>
 
@@ -22,49 +50,33 @@
             </div>
 
             <div class="cards">
-
                 <div class="card">
-
                     <i class="fa-solid fa-users"></i>
-
                     <div class="card-text">
-
-                        <span>320</span>
-
+                        <span>
+                            <?php echo $total_students; ?>
+                        </span>
                         <p>Students</p>
-
                     </div>
-
                 </div>
-
                 <div class="card">
-
                     <i class="fa-solid fa-id-card"></i>
-
                     <div class="card-text">
-
-                        <span>280</span>
-
+                        <span>
+                            <?php echo $total_cards; ?>
+                        </span>
                         <p>Cards</p>
-
                     </div>
-
                 </div>
-
                 <div class="card">
-
                     <i class="fa-solid fa-chalkboard"></i>
-
                     <div class="card-text">
-
-                        <span>15</span>
-
+                        <span>
+                            <?php echo $total_classes; ?>
+                        </span>
                         <p>Classes</p>
-
                     </div>
-
                 </div>
-
             </div>
 
         </div>
