218c2b8c95655dcdc935943a0cd9cf836defdd42
osm-zrl <78822717+osm-zrl@users.noreply.github.com>
2023-06-03 21:05:00 +0100

    modified:   edit.php 	modified:   student_info.php

--- edit.php
+++ edit.php
@@ -1,153 +1,174 @@
 <?php
 
 require('dbconfig.php');
+if (isset($_GET['student_id'])) {
 
-// retrieve the student_id parameter from the URL
-$student_id = $_GET['student_id'];
+    // retrieve the student_id parameter from the URL
+    $student_id = $_GET['student_id'];
 
-// retrieve the current student information from the database
+    // retrieve the current student information from the database
 // assuming you have a database connection already established
-$query = "SELECT * FROM student WHERE student_id = '$student_id'";
-$result = mysqli_query($conn, $query);
-$row = mysqli_fetch_assoc($result);
+    $query = "SELECT * FROM student WHERE student_id = '$student_id'";
+    $result = $conn->query($query);
+    if ($result->num_rows == 0) {
+        header('location:students.php');
+    }
+    $row = $result->fetch_assoc();
 
-// pre-populate the form fields with the student's current data
-$first_name = $row['first_name'];
-$last_name = $row['last_name'];
-$birthday = $row['birthday'];
-$gender = $row['gender'];
-$class_id = $row['class_id'];
+    // pre-populate the form fields with the student's current data
+    $first_name = $row['first_name'];
+    $last_name = $row['last_name'];
+    $birthday = $row['birthday'];
+    $gender = $row['gender'];
+    $class_id = $row['class_id'];
 
-// handle the form submission
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // retrieve the form data
-    $student_id = $_POST['student_id'];
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $birthday = $_POST['birthday'];
-    $gender = $_POST['gender'];
-    $class_id = $_POST['class_id'];
+    // handle the form submission
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // retrieve the form data
+        $student_id = $_POST['student_id'];
+        $first_name = $_POST['first_name'];
+        $last_name = $_POST['last_name'];
+        $birthday = $_POST['birthday'];
+        $gender = $_POST['gender'];
+        $class_id = $_POST['class_id'];
 
-    // prepare the SQL update statement
-    $query = "UPDATE student SET first_name=?, last_name=?, birthday=?, gender=?, class_id=? WHERE student_id=?";
-    $stmt = mysqli_prepare($conn, $query);
-    mysqli_stmt_bind_param($stmt, "ssssii", $first_name, $last_name, $birthday, $gender, $class_id, $student_id);
+        // prepare the SQL update statement
+        $query = "UPDATE student SET first_name=?, last_name=?, birthday=?, gender=?, class_id=? WHERE student_id=?";
+        $stmt = mysqli_prepare($conn, $query);
+        mysqli_stmt_bind_param($stmt, "ssssii", $first_name, $last_name, $birthday, $gender, $class_id, $student_id);
 
-    // execute the update statement
-    if (mysqli_stmt_execute($stmt)) {
-        // if the update was successful, redirect to the student details page
-        header("Location: students.php?student_id=$student_id");
-    } else {
-        // if there was an error, display an error message
-        echo "Error updating student: " . mysqli_error($conn);
+        // execute the update statement
+        if (mysqli_stmt_execute($stmt)) {
+            // if the update was successful, redirect to the student details page
+            header("Location: students.php?student_id=$student_id");
+        } else {
+            // if there was an error, display an error message
+            echo "Error updating student: " . mysqli_error($conn);
+        }
+
+        mysqli_stmt_close($stmt);
     }
 
-    mysqli_stmt_close($stmt);
-}
+    mysqli_close($conn);
 
-mysqli_close($conn);
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <?php require('head.php');?>
-    <title>main</title>
-    <link rel="stylesheet" href="css/student.css">
-</head>
-<body>
-    <?php require('aside.php');?>
-    <!-- top main title + statics cards  -->
-    <main>
-        <div class="top-main">
-            <div class="title">
-                <h2>Update Student</h2>
-                <h5>Here you can see all the student informations</h5>
-            </div>
-        </div>
-        <form class="information" action="" method="POST">
-            <div class="row">
-                <div class="col">
-                    <h4>Student CEF :</h4>
-                    <input type="number" name="student_id" value="<?php echo $student_id; ?>">
+    <head>
+        <?php require('head.php'); ?>
+        <title>main</title>
+        <link rel="stylesheet" href="css/student.css">
+    </head>
+
+    <body>
+        <?php require('aside.php'); ?>
+        <!-- top main title + statics cards  -->
+        <main>
+            <div class="top-main">
+                <div class="title">
+                    <h2>Update Student</h2>
+                    <h5>Here you can see all the student informations</h5>
                 </div>
             </div>
-            <div class="row">
-                <div class="col">
-                    <h4>Student First Name :</h4>
-                    <input type="text" name="first_name" value="<?php echo $first_name; ?>">
+            <form class="information" action="" method="POST">
+                <div class="row">
+                    <div class="col">
+                        <h4>Student CEF :</h4>
+                        <input type="number" name="student_id" value="<?php echo $student_id; ?>">
+                    </div>
                 </div>
-                <div class="col">
-                    <h4>Student Last Name :</h4>
-                    <input type="text" name="last_name" value="<?php echo $last_name; ?>">
+                <div class="row">
+                    <div class="col">
+                        <h4>Student First Name :</h4>
+                        <input type="text" name="first_name" value="<?php echo $first_name; ?>">
+                    </div>
+                    <div class="col">
+                        <h4>Student Last Name :</h4>
+                        <input type="text" name="last_name" value="<?php echo $last_name; ?>">
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <h4>Student Birthday :</h4>
-                    <input type="date" name="birthday" value="<?php echo $birthday; ?>">
+                <div class="row">
+                    <div class="col">
+                        <h4>Student Birthday :</h4>
+                        <input type="date" name="birthday" value="<?php echo $birthday; ?>">
+                    </div>
+                    <div class="col">
+                        <h4>Student Gender :</h4>
+                        <select name="gender" id="gender">
+                            <option value="">Select Gender</option>
+                            <option value="male" <?php if ($gender == 'male')
+                                echo ' selected'; ?>>Male</option>
+                            <option value="female" <?php if ($gender == 'female')
+                                echo ' selected'; ?>>Female</option>
+                        </select>
+                    </div>
                 </div>
-                <div class="col">
-                    <h4>Student Gender :</h4>
-                    <select name="gender" id="gender">
-                        <option value="">Select Gender</option>
-                        <option value="male"<?php if ($gender == 'male') echo ' selected'; ?>>Male</option>
-                        <option value="female"<?php if ($gender == 'female') echo ' selected'; ?>>Female</option>
-                    </select>
+                <div class="row">
+                    <div class="col">
+                        <h4>Student Class :</h4>
+                        <select name="class_id" id="class_id">
+                            <option value="">Select Student Group</option>
+                            <option value="1" <?php if ($class_id == 1)
+                                echo ' selected'; ?>>Développement digital 101
+                            </option>
+                            <option value="2" <?php if ($class_id == 2)
+                                echo ' selected'; ?>>Développement digital 102
+                            </option>
+                            <option value="3" <?php if ($class_id == 3)
+                                echo ' selected'; ?>>gestion entreprise 101</option>
+                            <option value="4" <?php if ($class_id == 4)
+                                echo ' selected'; ?>>gestion entreprise 102</option>
+                            <option value="5" <?php if ($class_id == 5)
+                                echo ' selected'; ?>>infographie</option>
+                        </select>
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <h4>Student Class :</h4>
-                    <select name="class_id" id="class_id">
-                        <option value="">Select Student Group</option>
-                        <option value="1"<?php if ($class_id == 1) echo ' selected'; ?>>Développement digital 101</option>
-                        <option value="2"<?php if ($class_id == 2) echo ' selected'; ?>>Développement digital 102</option>
-                        <option value="3"<?php if ($class_id == 3) echo ' selected'; ?>>gestion entreprise 101</option>
-                        <option value="4"<?php if ($class_id == 4) echo ' selected'; ?>>gestion entreprise 102</option>
-                        <option value="5"<?php if ($class_id == 5) echo ' selected'; ?>>infographie</option>
-                    </select>
-                </div>
-            </div>
-            <button type="submit">Update Student</button>
-        </form>
-    </main>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="js/chart.js"></script>
-    <script src="js/script.js"></script>
-</body>
-</html>
+                <button type="submit">Update Student</button>
+            </form>
+        </main>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <script src="js/chart.js"></script>
+        <script src="js/script.js"></script>
+    </body>
 
-<style>
-    /* student edit */
-.information {
-  background: white;
-  height: fit-content;
-  border-radius: 12px;
-  margin: 0rem;
-  padding: 2rem;
-  color: black;
-  display: flex;
-  flex-direction: column;
-  gap: 2rem;
-}
+    </html>
 
-.information h4 {
-  font-size: 1.3rem;
-  margin: 0;
-}
+    <style>
+        /* student edit */
+        .information {
+            background: white;
+            height: fit-content;
+            border-radius: 12px;
+            margin: 0rem;
+            padding: 2rem;
+            color: black;
+            display: flex;
+            flex-direction: column;
+            gap: 2rem;
+        }
 
-.col input {
-  width: 100%;
-}
+        .information h4 {
+            font-size: 1.3rem;
+            margin: 0;
+        }
 
-.col select#gender,.col select#classe {
-  width: 100%;
-}
+        .col input {
+            width: 100%;
+        }
 
-.information .col {
-    display: flex;
-    gap: 1rem;
-    flex-direction: column;
-}
-</style>
+        .col select#gender,
+        .col select#classe {
+            width: 100%;
+        }
+
+        .information .col {
+            display: flex;
+            gap: 1rem;
+            flex-direction: column;
+        }
+    </style>
+    <?php
+} else {
+    header('location:students.php');
+} ?>
--- student_info.php
+++ student_info.php
@@ -1,140 +1,151 @@
 <?php
 
 require('dbconfig.php');
-
-// retrieve the student_id parameter from the URL
-$student_id = $_GET['student_id'];
-
-// retrieve the current student information from the database
-// assuming you have a database connection already established
-$query = "SELECT student.*,concat(classe.name,' ',classe.level) as class_name FROM student JOIN classe ON student.class_id=classe.class_id WHERE student_id = '$student_id'";
-$result = mysqli_query($conn, $query);
-$row = mysqli_fetch_assoc($result);
-
-// pre-populate the form fields with the student's current data
-$first_name = $row['first_name'];
-$last_name = $row['last_name'];
-$birthday = $row['birthday'];
-$gender = $row['gender'];
-$class_id = $row['class_name'];
+if (isset($_GET['student_id'])){
 
 
+    // retrieve the student_id parameter from the URL
+    $student_id = trim($_GET['student_id']);
+    if ($student_id==''){
+        header('location:students.php');
+    }
+    // retrieve the current student information from the database
+    // assuming you have a database connection already established
+    $query = "SELECT student.*,concat(classe.name,' ',classe.level) as class_name FROM student JOIN classe ON student.class_id=classe.class_id WHERE student_id = '$student_id'";
+    $result = $conn->query($query);
+    if ($result->num_rows == 0){
+        header('location:students.php');
+    }
+    $row = $result->fetch_assoc();
 
-mysqli_close($conn);
+    // pre-populate the form fields with the student's current data
+    $first_name = $row['first_name'];
+    $last_name = $row['last_name'];
+    $birthday = $row['birthday'];
+    $gender = $row['gender'];
+    $class_id = $row['class_name'];
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <?php require('head.php');?>
-    <title>main</title>
-    <link rel="stylesheet" href="css/student.css">
-</head>
-<body>
-    <?php require('aside.php');?>
-    <!-- top main title + statics cards  -->
-    <main>
 
-        <div class="top-main">
-            <div class="title">
-                <h2>Update Student</h2>
-                <h5>Here you can see all the student informations</h5>
+
+    mysqli_close($conn);
+
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <?php require('head.php');?>
+        <title>main</title>
+        <link rel="stylesheet" href="css/student.css">
+    </head>
+    <body>
+        <?php require('aside.php');?>
+        <!-- top main title + statics cards  -->
+        <main>
+
+            <div class="top-main">
+                <div class="title">
+                    <h2>Update Student</h2>
+                    <h5>Here you can see all the student informations</h5>
+                </div>
             </div>
+            <button type="submit" id="edit">Edit Infos</button>
+            <div class="information">
+                <div class="row">
+                    <div class="col">
+                        <h4>Student CEF :</h4>
+                        <h6><?php echo $student_id; ?></h6>
+                    </div>
+                </div>
+                <div class="row gap-3">
+                    <div class="col">
+                        <h4>Student First Name :</h4>
+                        <h6><?php echo $first_name; ?></h6>
+                    </div>
+                    <div class="col">
+                        <h4>Student Last Name :</h4>
+                        <h6><?php echo $last_name; ?></h6>
+                    </div>
+                </div>
+                <div class="row gap-3">
+                    <div class="col">
+                        <h4>Student Birthday :</h4>
+                        <h6><?php echo $birthday; ?></h6>
+                    </div>
+                    <div class="col">
+                        <h4>Student Gender :</h4>
+                        <h6><?php echo $gender; ?></h6>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <h4>Student Class :</h4>
+                        <h6><?php echo $class_id; ?></h6>
+                    </div>
+                </div>
         </div>
-        <button type="submit" id="edit">Edit Infos</button>
-        <div class="information">
-            <div class="row">
-                <div class="col">
-                    <h4>Student CEF :</h4>
-                    <h6><?php echo $student_id; ?></h6>
-                </div>
-            </div>
-            <div class="row gap-3">
-                <div class="col">
-                    <h4>Student First Name :</h4>
-                    <h6><?php echo $first_name; ?></h6>
-                </div>
-                <div class="col">
-                    <h4>Student Last Name :</h4>
-                    <h6><?php echo $last_name; ?></h6>
-                </div>
-            </div>
-            <div class="row gap-3">
-                <div class="col">
-                    <h4>Student Birthday :</h4>
-                    <h6><?php echo $birthday; ?></h6>
-                </div>
-                <div class="col">
-                    <h4>Student Gender :</h4>
-                    <h6><?php echo $gender; ?></h6>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <h4>Student Class :</h4>
-                    <h6><?php echo $class_id; ?></h6>
-                </div>
-            </div>
-    </div>
 
-        
-    </main>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="js/chart.js"></script>
-    <script src="js/script.js"></script>
-    <script>
-         document.getElementById('edit').addEventListener('click', function() {
-        location.href = 'edit.php?student_id=<?php echo $student_id; ?>';
-    });
-    </script>
-</body>
-</html>
+            
+        </main>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <script src="js/chart.js"></script>
+        <script src="js/script.js"></script>
+        <script>
+            document.getElementById('edit').addEventListener('click', function() {
+            location.href = 'edit.php?student_id=<?php echo $student_id; ?>';
+        });
+        </script>
+    </body>
+    </html>
 
-<style>
-    /* student edit */
-.information {
-  background: white;
-  height: fit-content;
-  border-radius: 12px;
-  margin: 0rem;
-  padding: 2rem;
-  color: black;
-  display: flex;
-  flex-direction: column;
-  gap: 2rem;
-}
-
-.information h4 {
-  font-size: 1rem;
-  margin: 0;
-  font-weight: lighter;
-  color:var(--grey-color);
-  padding:10px 0;
-  background-color: var(--third-color);
-  padding-left: 5px;
-}
-.information h6{
-    margin:auto;
-    padding:10px 0;
-
-}
-
-.col input {
-  width: 100%;
-}
-
-.col select#gender,.col select#classe {
-  width: 100%;
-}
-
-.information .col {
+    <style>
+        /* student edit */
+    .information {
+    background: white;
+    height: fit-content;
+    border-radius: 12px;
+    margin: 0rem;
+    padding: 2rem;
+    color: black;
     display: flex;
     flex-direction: column;
-    padding:0;
-    background-color: var(--grey-color);
-   
-}
-#edit{
-    font-weight: bold;
-}
-</style>
+    gap: 2rem;
+    }
+
+    .information h4 {
+    font-size: 1rem;
+    margin: 0;
+    font-weight: lighter;
+    color:var(--grey-color);
+    padding:10px 0;
+    background-color: var(--third-color);
+    padding-left: 5px;
+    }
+    .information h6{
+        margin:auto;
+        padding:10px 0;
+
+    }
+
+    .col input {
+    width: 100%;
+    }
+
+    .col select#gender,.col select#classe {
+    width: 100%;
+    }
+
+    .information .col {
+        display: flex;
+        flex-direction: column;
+        padding:0;
+        background-color: var(--grey-color);
+    
+    }
+    #edit{
+        font-weight: bold;
+    }
+    </style>
+<?php
+}else{
+    header('location:students.php');
+  }  ?>
